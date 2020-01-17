50187879c062c59ac6f6012028a9061ff7bd57fa
Inês Filipe <ines.filipe@tecnico.ulisboa.pt>
2020-01-17 00:21:58 +0000

    update backup assets

--- project/cgj/cgj/assets/shaders/rain-particle-gs.glsl
+++ project/cgj/cgj/assets/shaders/rain-particle-gs.glsl
@@ -8,18 +8,22 @@ uniform Camera {
 	mat4 ProjectionMatrix;
 };
 
+uniform mat4 ModelMatrix;
+
 in vec3 gColor[];
+in float gSize[];
 
 out vec4 fColor;
 
 void main()
 {
-	fColor = vec4(gColor[0], 0.2);
+	fColor = vec4(gColor[0], 0.1);
+	float Size = gSize[0];
 	vec4 p = gl_in[0].gl_Position;
 
-	gl_Position = ProjectionMatrix * p;
+	gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * p;
 	EmitVertex();
-	gl_Position = ProjectionMatrix * vec4(p.x, p.y - 0.1, p.zw);
+	gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(p.x, p.y - Size, p.zw);
 	EmitVertex();
 	EndPrimitive();
 }
